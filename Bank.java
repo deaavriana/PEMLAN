ca1c738aceb49474ca96d09825bf507242f0938a
deaavriana <deaavriana@gmail.com>
2026-03-12 10:47:04 +0700

    Bank.java

--- Bank.java
+++ Bank.java
@@ -1,19 +1,22 @@
 public class Bank {
+
     private String namaAkun;
     private String password;
     private String nomorRekening;
     private double saldo;
 
-    // Constructor
     public Bank(String namaAkun, String password, String nomorRekening, double saldoAwal) {
+
+        // mengecek apakah password memiliki huruf besar
+        boolean adaHurufBesar = !password.equals(password.toLowerCase());
 
         if (namaAkun.length() < 4) {
             System.out.println("Nama akun tidak boleh kurang dari 4 huruf");
             return;
         }
 
-        if (password.length() < 8 || !password.contains("_")) {
-            System.out.println("Password minimal 8 karakter dan harus ada _");
+        if (password.length() < 8 || !password.contains("_") || !adaHurufBesar) {
+            System.out.println("Password minimal 8 karakter, harus ada _, dan minimal 1 huruf besar");
             return;
         }
 
@@ -32,24 +35,25 @@ public class Bank {
         this.nomorRekening = nomorRekening;
         this.saldo = saldoAwal;
 
-        System.out.println("\nAkun berhasil dibuat");
+        System.out.println("Akun berhasil dibuat");
         tampilkanInfo();
     }
 
-    // Method menambah saldo
     public void tambahSaldo(double jumlah, String pw) {
+
         if (!pw.equals(password)) {
             System.out.println("\nPassword salah, transaksi gagal\n");
             return;
         }
 
         saldo += jumlah;
+
         System.out.println("\nSaldo berhasil ditambah: " + String.format("%.2f", jumlah));
         System.out.println("Saldo sekarang: " + String.format("%.2f", saldo) + "\n");
     }
 
-    // Method mengurangi saldo
     public void tarikSaldo(double jumlah, String pw) {
+
         if (!pw.equals(password)) {
             System.out.println("\nPassword salah, transaksi gagal\n");
             return;
@@ -61,14 +65,15 @@ public class Bank {
         }
 
         saldo -= jumlah;
+
         System.out.println("\nSaldo berhasil ditarik: " + String.format("%.2f", jumlah));
         System.out.println("Saldo sekarang: " + String.format("%.2f", saldo) + "\n");
     }
 
-    // Method menampilkan info akun
     public void tampilkanInfo() {
         System.out.println("Nama Akun      : " + namaAkun);
         System.out.println("No Rekening    : " + nomorRekening);
-        System.out.println("Saldo          : " + String.format("%.2f", saldo) + "\n");
+        System.out.println("Saldo          : " + String.format("%.2f", saldo));
+        System.out.println();
     }
 }
