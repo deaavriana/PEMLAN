a82dd858c2a0cf7b0877f36bdc26cf61acb47a4f
deaavriana <deaavriana@gmail.com>
2026-03-12 10:46:30 +0700

    MainBank.java

--- MainBank.java
+++ MainBank.java
@@ -1,15 +1,16 @@
 public class MainBank {
+
     public static void main(String[] args) {
 
         Bank akun1 = new Bank(
                 "DeaErsa",
-                "pass_word",
+                "Pass_word",
                 "1234567890",
                 500000);
 
-        akun1.tambahSaldo(200000, "pass_word");
-        akun1.tarikSaldo(100000, "pass_word");
-        akun1.tarikSaldo(700000, "pass_word"); // saldo tidak cukup
-        akun1.tambahSaldo(50000, "salah_pw"); // password salah
+        akun1.tambahSaldo(200000, "Pass_word");
+        akun1.tarikSaldo(100000, "Pass_word");
+        akun1.tarikSaldo(700000, "Pass_word");
+        akun1.tambahSaldo(50000, "Salah_pw");
     }
 }
